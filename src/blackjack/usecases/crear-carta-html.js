45df03b6b7a112c307685f11576c6b8671af51ba
Daniel <danielfrancisco12@gmail.com>
2025-03-31 03:25:54 -0500

    Apartado visual mejorado

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -1,17 +1,15 @@
-
 /**
- * 
- * @param {String} carta 
+ *
+ * @param {String} carta
  * @returns {HTMLImageElement} imagen de retorno
  */
-export const crearCartaHTML = ( carta ) => {
+export const crearCartaHTML = (carta) => {
+  if (!carta) throw new Error("La carta es un argumento obligatorio");
 
-    if ( !carta ) throw new Error('La carta es un argumento obligatorio');
+  // <img class="carta" src="assets/cartas/2C.png">
+  const imgCarta = document.createElement("img");
+  imgCarta.src = `public/assets/cartas/${carta}.png`; //3H, JD
+  imgCarta.classList.add("carta");
 
-    // <img class="carta" src="assets/cartas/2C.png">
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
-    imgCarta.classList.add('carta');
-
-    return imgCarta;
-}
+  return imgCarta;
+};
